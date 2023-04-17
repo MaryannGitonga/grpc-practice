8df685e199a0b8e05bad0f9a07611d4a1e7a189a
MaryannGitonga <naygitonga4@gmail.com>
2023-04-17 12:16:58 +0300

    Add client

---
+++ client.js
@@ -0,0 +1,27 @@
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader")
+const packageDef = protoLoader.loadSync("todo.proto", {});
+const grpcObject = grpc.loadPackageDefinition(packageDef)
+const todoPackage = grpcObject.todoPackage;
+
+// add todos as command line arguments
+const text = process.argv[2];
+
+const client = new todoPackage.Todo("localhost:40000", grpc.credentials.createInsecure())
+
+client.createTodo({"text": text}, (err, response) => {
+    console.log("Created todo...", JSON.stringify(response.text))
+});
+
+// client.readTodos({}, (err, response) => {
+//     if (!response.items)
+//         response.items.forEach(i => { console.log(i.text) });
+// })
+
+const call = client.readTodosStream()
+call.on("data", item => {
+    console.log("Received item from server....", JSON.stringify(item));
+});
+
+// define the end protocol when the server ends the communication
+call.on("end", e => console.log("server done!"));
